1fc0477e1170797a22f9cd5522cf02fc9a475c87
hpnightowl <iamhp2k@gmail.com>
2021-05-03 21:41:49 +0530

    cs: dummy return for model

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+@app.route("/")
+def home():
+  ps = pd.read_csv('testdata/datatest.csv')
+  model = pickle.load(open("model/model.pkl", "rb"))
+  kmeans_clusters = model.predict(ps)
+  print(kmeans_clusters)
+  return str(kmeans_clusters)
 
 if __name__ == '__main__':
     app.run()
