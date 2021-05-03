1f52d3e3cec671fccf648dd0f9149a512c9383d5
hpnightowl <iamhp2k@gmail.com>
2021-05-03 21:48:13 +0530

    cs: make it more generic
    *remove debug variables

--- app.py
+++ app.py
@@ -8,9 +8,16 @@ app = Flask(__name__)
 def home():
   ps = pd.read_csv('testdata/datatest.csv')
   model = pickle.load(open("model/model.pkl", "rb"))
-  kmeans_clusters = model.predict(ps)
-  print(kmeans_clusters)
-  return str(kmeans_clusters)
+  prediction = model.predict(ps)
+  if float(prediction) == 0:
+    cluster = 'BOGO offers'
+  elif float(prediction) == 1:
+    cluster = 'Receive a higher than average number of offers'
+  elif float(prediction) == 2:
+    cluster = 'no BOGO offers'
+  elif float(prediction) == 3:
+    cluster = 'Regular offers'
+  return str(cluster)
 
 if __name__ == '__main__':
     app.run()
